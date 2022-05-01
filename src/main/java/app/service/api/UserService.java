0f709cabf0d6bb4e2793931a92f0a5c9453a63b7
Keresztes Beata <keresztesbeata00@yahoo.com>
2022-05-01 08:03:33 +0300

    generated javadocs

--- src/main/java/app/service/api/UserService.java
+++ src/main/java/app/service/api/UserService.java
@@ -25,7 +25,6 @@ public interface UserService {
      * @param userDto contains all the relevant user related information.
      * @throws InvalidDataException when either the username or the password is missing or invalid
      * @throws DuplicateDataException when the username is already taken
-     * @pre username != null && password != null
      */
     void addUser(UserDto userDto) throws InvalidDataException, DuplicateDataException;
 }
